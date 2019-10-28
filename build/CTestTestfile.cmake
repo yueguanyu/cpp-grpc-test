fa98c203761fe26f0ba51d53716e4d5a8afc00cb
guanyu <yue.guanyu@hotmail.com>
2019-10-28 13:04:27 +0800

    change gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/guanyuyue/Develop/gitstore/cpp-grpc-test
-# Build directory: /Users/guanyuyue/Develop/gitstore/cpp-grpc-test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
